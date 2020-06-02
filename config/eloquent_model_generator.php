1de293c193bf376dea98cdf5a3a2481a7cd71cce
Guilherme Cabo <guilherme.pcabo@gmail.com>
2020-06-02 15:13:42 -0300

    Removed IDE files

--- config/eloquent_model_generator.php
+++ config/eloquent_model_generator.php
@@ -3,7 +3,7 @@
 return [
     'model_defaults' => [
         'namespace' => 'App\\Models',
-        'base_class_name' => \Illuminate\Database\Eloquent\Model::class,
+        'base_class_name' => \App\BaseModel::class,
         'output_path' => "./Models",
         'no_timestamps' => null,
         'date_format' => null,
